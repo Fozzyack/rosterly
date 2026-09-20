74de090cee07394b68b8d1e91ea799c9f0a2badd
Frasier <fsundra@gmail.com>
2026-09-20 19:14:56 +0800

    sets up server

--- backend/main.go
+++ backend/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"net/http"
+	"time"
 
 	"github.com/joho/godotenv"
 )
@@ -14,9 +17,21 @@ func main() {
 		log.Fatal("Error Loading .env file")
 	}
 
-	var port int;
+	var port int
 	flag.IntVar(&port, "port", 8000, "server address port")
 	flag.Parse()
+
+	server := &http.Server{
+		Addr:         fmt.Sprintf(":%d", port),
+		WriteTimeout: time.Second * 5,
+		ReadTimeout:  time.Second * 10,
+		IdleTimeout:  time.Second * 60,
+	}
+
+	err = server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	return
 }
