6904feef68ca72d87628ed687bb1d20a9a200c99
Frasier <fsundra@gmail.com>
2026-09-20 18:49:31 +0800

    init backend

--- backend/main.go
+++ backend/main.go
@@ -1,10 +1,22 @@
-package main 
+package main
 
-import "fmt"
+import (
+	"flag"
+	"log"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello World");
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error Loading .env file")
+	}
+
+	var port int;
+	flag.IntVar(&port, "port", 8000, "server address port")
+	flag.Parse()
 
 	return
 }
-
