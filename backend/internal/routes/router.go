7322b3be2bd6d9d4d4bf8328633d23963352f750
Frasier <fsundra@gmail.com>
2026-09-20 22:36:47 +0800

    adds create route

--- backend/internal/routes/router.go
+++ backend/internal/routes/router.go
@@ -14,8 +14,8 @@ func SetupRoutes(app *app.Application) *chi.Mux {
 	r.Use(middleware.RequestID)
 	r.Use(middleware.Recoverer)
 
-	r.Get("/health", app.HealthHandler.GetHealth)
-	r.Post("/users", app.UserHandler.CreateUser)
+	r.Get("/health/", app.HealthHandler.GetHealth)
+	r.Post("/users/", app.UserHandler.CreateUser)
 
 	return r
 
